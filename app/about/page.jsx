44f5e6620791d332bc9e5f30300914e541bf1641
vmaarcos <111014095+vmaarcos@users.noreply.github.com>
2025-06-04 12:52:04 -0300

    Update page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -32,10 +32,10 @@ export default function About() {
           >
             <h1>Hello World, Eu sou Marcos!</h1>
             <p>
-            Meu nome é Marcos Vinicios, tenho 19 anos e nasci em 2006. sempre morei na periferia. 
+            Meu nome é Marcos Vinicios, nasci em 2006 e sempre morei na periferia. 
             Nasci em Taboão da Serra, mas atualmente moro em Ferraz de Vasconcelos, no extremo leste de São Paulo. 
             Aos 17 anos, no meu último ano do ensino médio, vi na tecnologia uma oportunidade de transformar minha vida. 
-            Sempre gostei de passar o dia no computador, então foi algo que se encaixou perfeitamente.
+            Sempre gostei de passar o dia todono computador, então foi algo que se encaixou perfeitamente.
             Além da programação, sou apaixonado por esportes — pratico corrida, academia e boxe. E claro, 
             nunca dispenso uma viagem para a praia!
             </p>
@@ -52,8 +52,7 @@ export default function About() {
           >
             <p>
             Em geral trabalho com Python, SQL Server, gráficos, e tomadas de decisões estratégicas e analíticas. 
-            também adoro projetos em NextJS e TailwindCSS e backend com api. 
-            Coisas que embora ainda não esteja no nível que desejo, acho que um dia chegarei ao nível que desejo.
+            também adoro projetos em NextJS e TailwindCSS.
             </p>
             <p>
             Quando não estou no quarto ou no escritorio estudando ou trabalhando, 
@@ -71,9 +70,8 @@ export default function About() {
             className="flex flex-col mb-4 lg:-mb-20 gap-6 animate-in"
             style={{ "--index": 8 }}
           >
-           Como mencionei, comecei em 2023 com o PROA. Tive a sorte de ter amigos muito experientes, no nível pleno
-           , o que me ajudou a me desenvolver mais rápido. Em 6 meses, 
-           já estava trabalhando com Next.js, JSX, TailwindCSS, Java com Spring Boot, e também explorando React Native e cibersegurança.
+           Como mencionei, comecei em 2023 com o PROA. Tive a sorte de ter amigos muito experientes, O que me ajudou a me desenvolver mais rápido. Em 6 meses, 
+           já estava trabalhando com Next.js, TailwindCSS, Java com Spring Boot, e também explorando React Native e cibersegurança.
           </div>
         </Section>
       </div>
